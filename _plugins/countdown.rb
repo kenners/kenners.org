346d29b082237c675a795c6d907be2cc01a604e5
Kenrick Turner <kenrickturner@gmail.com>
2013-07-30 18:14:37 +0100

    Adding countdown

---
+++ _plugins/countdown.rb
@@ -0,0 +1,22 @@
+require 'date'
+
+module Jekyll
+  module CountdownFilter
+    def countdown(date)
+      dday = Jekyll.configuration({})['countdown']['dday']
+      if dday > date.to_date
+        days = (dday - date.to_date).to_i
+        if days == 1
+            "Deploy in #{days} day"
+        else
+            "Deploy in #{days} days"
+        end
+      else
+        days = (date.to_date - dday).to_i
+        "Day \##{days} in Antarctica"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CountdownFilter)
