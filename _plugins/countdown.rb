9e94b58cee4c6ac0dc0ed22002985c74a774cfb8
Kenrick Turner <kenrickturner@gmail.com>
2013-11-17 20:47:06 -0300

    Fixing countdown

--- _plugins/countdown.rb
+++ _plugins/countdown.rb
@@ -4,6 +4,7 @@ module Jekyll
   module CountdownFilter
     def countdown(date)
       dday = Jekyll.configuration({})['countdown']['dday']
+      arrivalday = Jekyll.configuration({})['countdown']['arrivalday']
       if dday > date.to_date
         days = (dday - date.to_date).to_i
         if days == 1
@@ -12,8 +13,12 @@ module Jekyll
             "Deploy in #{days} days"
         end
       else
-        days = (date.to_date - dday).to_i
-        "Day \##{days} South"
+        days = (date.to_date - arrivalday).to_i
+        if arrivalday > date.to_date
+          "In transit"
+        else
+          "Day \##{days} in Antarctica"
+        end
       end
     end
   end
